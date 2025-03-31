74562f79a0412e3367aecd4eda12a5b8c7742e81
IntelliJEclipse <bko2369@hanmail.net>
2025-03-31 17:03:15 +0900

    add: 로그인/로그아웃, customer page, metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,33 @@
-export default function Page() {
-    return <p>customer page </p>
+import Table from '../../ui/customers/table'
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+ 
+export const metadata:Metadata={
+    title:'customer'
+  }
+
+export default async function Page(props:{
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const customers = await fetchFilteredCustomers(query);
+    // const totalPages = await fetchInvoicesPages(query);
+    
+  return (
+    <div className="w-full">
+       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table customers={customers}/>
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
 }
